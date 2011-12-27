3f7a8724f88552956d29ffb72ba4018004160a8e
Valery Dubrava <valery.dubrava@gmail.com>
2011-12-27 12:26:08 +0400

    main window size fixed

--- htdocs/js/vkutils.js
+++ htdocs/js/vkutils.js
@@ -7,6 +7,10 @@
  * Copyright(c) 2010-2011 Val Dubrava [ valery.dubrava@gmail.com ] 
  * 
  */
+// main iframe size
+var mainWidth = 650;
+
+
 VK.init(function() {
     VK.api('getUserSettings', {test_mode: 1}, function(data) {
         if ((data.response & 8) !== 8) {
@@ -16,18 +20,17 @@ VK.init(function() {
     
     VK.Widgets.Comments('vk_comments', {
         limit: 25, 
-        width: '800', 
+        width: mainWidth, 
         attach: '*'
     });
     
 //    VK.Widgets.Group("vk_groups", {
 //        mode: 0, 
-//        width: "800", 
+//        width: mainWidth, 
 //        height: "290"
 //    }, 33532766)
 });
 
-var mainWidth = 800;
 
 function mainResize(height) {
     VK.callMethod('resizeWindow', mainWidth, height);
