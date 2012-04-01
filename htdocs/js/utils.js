6ac3b5d6947356c880cbaa552404f44f78f5bee3
Valery Dubrava <valery.dubrava@gmail.com>
2012-04-02 01:09:19 +0400

    catalogue completed

--- htdocs/js/utils.js
+++ htdocs/js/utils.js
@@ -85,4 +85,22 @@
             return err.stack;
         }
     }
+    $.fn.setUid = function() {
+        return $(this).each(function (id, el) {
+            $(el).attr('id', $.genUuid());
+        });
+    }
+
+    $.genUuid = function() {
+        var uuid = "", i, random;
+        for (i = 0; i < 32; i++) {
+            random = Math.random() * 16 | 0;
+
+            if (i == 8 || i == 12 || i == 16 || i == 20) {
+                uuid += "-"
+            }
+            uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
+        }
+        return uuid;
+    }
 })(jQuery)
